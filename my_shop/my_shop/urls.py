0dbbe9e601d1820c0f35e5d6dc3c550b3967068b
Idel Yusupov <idel84@mail.ru>
2021-10-12 00:28:19 +0300

    lesson_3

--- my_shop/my_shop/urls.py
+++ my_shop/my_shop/urls.py
@@ -1,12 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import main, contacts
-
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
     path('contacts/', contacts, name='contacts'),
     path('products/', include('mainapp.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
