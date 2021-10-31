39aed8c3a83f08c440e7f82d87dd08a1755a65d9
Idel Yusupov <idel84@mail.ru>
2021-11-01 00:39:17 +0300

    lesson_8(paginator)

--- my_shop/mainapp/views.py
+++ my_shop/mainapp/views.py
@@ -1,4 +1,6 @@
 import random
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from basketapp.models import Basket
 from mainapp.models import ProductCategory, Product
@@ -20,32 +22,50 @@ def get_same_products(hot_product):
     return same_products
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'Каталог'
-
     links_menu = ProductCategory.objects.all()
-    products = Product.objects.all()[1:4]
     basket = get_basket(request.user)
 
     if pk is not None:
         if pk == 0:
-            products = Product.objects.all()[1:4]
-            category = {'name': 'все'}
+            products = Product.objects.filter(
+                is_active=True,
+                category__is_active=True
+            ).order_by('-price')
+            category = {'pk': 0, 'name': 'все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('price')
+            products = Product.objects.filter(
+                category__pk=pk,
+                is_active=True,
+                category__is_active=True
+            ).order_by('-price')
+
+        paginator = Paginator(products, 2)
+        try:
+            product_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            product_paginator = paginator.page(1)
+        except EmptyPage:
+            product_paginator = paginator.page(paginator.num_pages)
 
         context = {
             'title': title,
             'links_menu': links_menu,
             'category': category,
-            'products': products,
+            'products': product_paginator,
             'basket': basket,
+            'paginator': paginator,
         }
         return render(request, 'mainapp/products.html', context)
 
     hot_product = get_hot_product()
     same_products = get_same_products(hot_product)
+    products = Product.objects.filter(
+        is_active=True,
+        category__is_active=True
+    ).order_by('-price')[:3]
 
     context = {
         'title': title,
@@ -62,6 +82,7 @@ def products(request, pk=None):
 def product(request, pk):
     title = 'Товар'
     product = get_object_or_404(Product, pk=pk)
+
     context = {
         'title': title,
         'links_menu': ProductCategory.objects.all(),
