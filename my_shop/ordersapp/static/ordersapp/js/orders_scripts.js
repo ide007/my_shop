d052cd0507e341c4d5a2d80e7e5ff90140d963aa
Idel Yusupov <idel84@mail.ru>
2021-11-18 23:19:37 +0300

    Django_2.4

--- my_shop/ordersapp/static/ordersapp/js/orders_scripts.js
+++ my_shop/ordersapp/static/ordersapp/js/orders_scripts.js
@@ -12,5 +12,53 @@ window.onload = function () {
         _quantity = parseInt($('input[name="orderitems-' + i + '-quantity"]').val());
         _price = parseFloat($('.ordersitems-' + i + '-price').val().replace(',', '.'));
 
-    }
+        quantity_arr.push(_quantity);
+        if(_price) {
+            price_arr.push(_price);
+        } else {
+            price_arr.push(0);
+        }
+        $('.order_form').on('click', 'input[type="number"]', funtion (){
+            var target = event.target;
+            orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+            if (price_arr[orderitem_num]){
+                orderitem_quantity = parseInt(target.value);
+                delta_quantity = orderitem_quantity - quantity_arr[orderitem_num];
+                quantity_arr[orderitem_num] = orderitem_quantity;
+                orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+            }
+        });
+
+        $('.order_form').on('click', 'input[type="checkbox"]', funtion (){
+            var target = event.target;
+            orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+            if (target.checked){
+                delta_quantity = -quantity_arr[orderitem_num];
+            } else {
+                delta_quantity = quantity_arr[orderitem_num]
+            }
+        });
+
+        function orderSummaryUpdate(orderitem_price, delta_quantity){
+            delta_cost = orderitem_price * delta_quantity;
+            order_total_price = Number((order_total_price + delte_cost).toFixed(2));
+            order_total_quantity = order_total_quantity + delta_quantity;
+
+            $('.order_total_quantity').html(order_total_quantity.toString())
+            $('.order_total_cost').html(orderitem_price.toString() + ',00');
+        }
+   }
+    function deleteOrderItem(row){
+        var target_name = row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+        }
+
+    $('formset_row').format({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        perfix: 'orderitems',
+        remove: deleteOrderItem,
+        });
 };
