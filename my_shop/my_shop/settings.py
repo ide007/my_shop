0caef0885046c71a5f068ebe3e9bfe991f999613
Idel Yusupov <idel84@mail.ru>
2021-11-26 00:31:29 +0300

    Django_2.5()

--- my_shop/my_shop/settings.py
+++ my_shop/my_shop/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -88,17 +88,16 @@ WSGI_APPLICATION = 'my_shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'NAME': 'my_shop',
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'USER': 'dbadmin2',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'NAME': 'my_shop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'db2admin',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
