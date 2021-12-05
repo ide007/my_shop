fb96efe8a8e76d9c94cb9d45fef56719d0d630a0
Idel Yusupov <idel84@mail.ru>
2021-12-05 10:40:35 +0300

    Django_2.6(finally)

--- my_shop/mainapp/views.py
+++ my_shop/mainapp/views.py
@@ -32,7 +32,7 @@ def products(request, pk=None, page=1):
             products = Product.objects.filter(
                 is_active=True,
                 category__is_active=True
-            ).order_by('-price')
+            ).select_related('category')
             category = {'pk': 0, 'name': 'все'}
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
@@ -40,7 +40,7 @@ def products(request, pk=None, page=1):
                 category__pk=pk,
                 is_active=True,
                 category__is_active=True
-            ).order_by('-price')
+            ).select_related('category')
 
         paginator = Paginator(products, 2)
         try:
@@ -64,7 +64,7 @@ def products(request, pk=None, page=1):
     products = Product.objects.filter(
         is_active=True,
         category__is_active=True
-    ).order_by('-price')[:2]
+    ).select_related('category')[:2]
     paginator = Paginator(products, 2)
     try:
         product_paginator = paginator.page(page)
